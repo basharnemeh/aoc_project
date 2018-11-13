4f22b596b860c687970259f46ccf4e1861ac9bab
Bahsar NEMEH <nemeh.bashar@gmail.com>
2018-11-13 16:25:25 +0100

    GenrateurImpl

--- src/main/java/aoc/generator/GenerateurImpl.java
+++ src/main/java/aoc/generator/GenerateurImpl.java
@@ -5,9 +5,9 @@ import java.util.List;
 import java.util.Random;
 
 import aoc.observer.ObserverAsync;
+import aoc.strategy.AlgoDiffusion;
 
-public class GenerateurImpl implements Generateur{
-	
+public class GenerateurImpl implements Generateur {
 
 	/**
 	 * Les observateurs du générateur Proxy pour les afficheurs
@@ -18,18 +18,36 @@ public class GenerateurImpl implements Generateur{
 	 * Valeur courante générée
 	 */
 	private Integer value;
-	
+
+	/**
+	 * Algorithme de diffusion courant
+	 */
+	private AlgoDiffusion diffusion;
+
+
+	// Générateur de valeurs
+	private Random rand;
+
+	/**
+	 * Variable de contrôle pour allumer/éteindre le générateur
+	 */
+	private boolean turnMeOn;
+
 	/**
 	 * Constructor
 	 */
 	public GenerateurImpl() {
 		this.canaux = new ArrayList<ObserverAsync<Generateur>>();
+		this.rand = new Random();
+		this.turnMeOn = true;
 	}
-	
+
 	/**
-	 * Ajoute un observateur aux observateurs de ce sujet s'il n'est pas déjà présent
-	 * Ne fait rien sinon
-	 * @param ObserverAsync<Generateur> o observateur à ajouter
+	 * Ajoute un observateur aux observateurs de ce sujet s'il n'est pas déjà
+	 * présent Ne fait rien sinon
+	 * 
+	 * @param ObserverAsync<Generateur>
+	 *            o observateur à ajouter
 	 */
 	@Override
 	public void attach(ObserverAsync<Generateur> o) {
@@ -37,11 +55,13 @@ public class GenerateurImpl implements Generateur{
 			this.canaux.add(o);
 		}
 	}
-	
+
 	/**
-	 * Retire un observateur aux observateurs de ce sujet s'il est présent
-	 * Ne fait rien sinon
-	 * @param ObserverAsync<Generateur> o observateur à retirer
+	 * Retire un observateur aux observateurs de ce sujet s'il est présent Ne fait
+	 * rien sinon
+	 * 
+	 * @param ObserverAsync<Generateur>
+	 *            o observateur à retirer
 	 */
 	@Override
 	public void detach(ObserverAsync<Generateur> o) {
@@ -49,7 +69,7 @@ public class GenerateurImpl implements Generateur{
 			this.canaux.remove(o);
 		}
 	}
-	
+
 	/**
 	 * Retourne la valeur actuelle générée par le générateur
 	 */
@@ -61,6 +81,59 @@ public class GenerateurImpl implements Generateur{
 	@Override
 	public List<ObserverAsync<Generateur>> getObservers() {
 		return new ArrayList<>(this.canaux);
+	}
+
+	/**
+	 * Définit l'algorithme de diffusion comme étant celui passé en paramètres
+	 * 
+	 * @param AlgoDiffusion
+	 *            algo algorithme de diffusion
+	 */
+	public void addStrategy(AlgoDiffusion algo) {
+		diffusion = algo;
+		diffusion.configure(this);
+	}
+
+	/**
+	 * Désactive l'algorithme de diffusion
+	 * 
+	 * @param AlgoDiffusion
+	 *            algo algorithme de diffusion
+	 */
+	public void removeStrategy(AlgoDiffusion algo) {
+		diffusion.configure(null);
+		diffusion = null;
+	}
+
+	/**
+	 * Notifie les observateurs suivant la cohérence/stratégie choisie
+	 */
+	public void notifyObservers() {
+		diffusion.execute();
+	}
+
+	/**
+	 * Génère aléatoirement une valeur
+	 */
+	public void generate() {
+		if (turnMeOn) {
+			this.value = rand.nextInt(100);
+			notifyObservers();
+		}
+	}
+
+	/**
+	 * Active le générateur
+	 */
+	public void start() {
+		turnMeOn = true;
+	}
+
+	/**
+	 * Arrête le générateur
+	 */
+	public void stop() {
+		turnMeOn = false;
 	}
 
 }
